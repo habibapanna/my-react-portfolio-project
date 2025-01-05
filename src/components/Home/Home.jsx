cc3f1cc5175b2e213e1f78bbf84ebd675838edff
habibapanna <habibapanna49@gmail.com>
2025-01-05 15:20:05 +0600

    new section added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,11 +8,30 @@ import BestProjects from '../BestProjects/BestProjects';
 const Home = () => {
     return (
         <div>
-           <Banner></Banner>
-           <Skills></Skills>
-           <Tools></Tools>
-           <BestProjects></BestProjects>
-           <Contact></Contact>
+            <Banner></Banner>
+            <Skills></Skills>
+            <Tools></Tools>
+            <BestProjects></BestProjects>
+
+            {/* Image Background Section */}
+            <section
+                className="relative h-screen bg-fixed w-full bg-center"
+                style={{
+                    backgroundImage: "url('https://i.postimg.cc/wjngwRSQ/person-front-computer-working-html.jpg')",
+                    backgroundRepeat: "no-repeat"
+                     
+                }}
+            >
+                {/* Optional Overlay for Better Contrast */}
+                <div className="absolute inset-0 bg-black bg-opacity-50"></div>
+                <div className="relative z-10 flex items-center justify-center h-full">
+                    <h2 className="text-white text-5xl font-bold">
+                        Creativity at Its Best
+                    </h2>
+                </div>
+            </section>
+
+            <Contact></Contact>
         </div>
     );
 };
